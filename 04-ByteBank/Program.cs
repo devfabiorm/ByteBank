fa738b3e3b9151d1abd6446c062cd3355e438cea
devfabiorm <fabio.evangelion.guitar@gmail.com>
2020-12-27 21:07:56 -0300

    Entendendo namespaces, composição de classes e tipo null

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+
+            contaDoBruno.Depositar(500);
+
+            Console.WriteLine("Saldo da conta do Bruno: R$" + contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            bool resultadoTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine("Saldo da conta do Bruno: R$" + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da conta da Gabriela: R$" + contaDaGabriela.saldo);
+
+            Console.WriteLine("Resultado da transferência " + resultadoTransferencia);
+
+            Console.ReadLine();
+        }
+    }
+}

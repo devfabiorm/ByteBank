266600778cc9e6ffd8a828fe42c2306da6e1fc5e
devfabiorm <fabio.evangelion.guitar@gmail.com>
2021-01-05 23:09:43 -0300

    Tirando o set das propriedades para criar um campo readonly

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        public static double TaxaOperacao { get; private set; }
+        public static int TotalDeContasCriadas { get; private set; }
+        public Cliente Titular { get; set; }
+        private int _agencia;
+        public int Agencia { get; }
+        public int Numero { get; }
+
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return this._saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    this._saldo = value;
+                }
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+            TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
+}
